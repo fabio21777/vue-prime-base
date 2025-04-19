578b49083dc1586a19ad9e98ba81b1a89798bd1e
Fabricio Souza <fabriciosouza21777@gmail.com>
2025-04-19 16:06:45 -0300

    #tema escuro inicial

--- src/main.ts
+++ src/main.ts
@@ -47,11 +47,39 @@ const myPresent = definePreset(Aura, {
 			light: {
 				formField: {
 					hoverBorderColor: '{primary.color}'
+				},
+				surface: {
+					0: '#ffffff',
+					50: '{zinc.50}',
+					100: '{zinc.100}',
+					200: '{zinc.200}',
+					300: '{zinc.300}',
+					400: '{zinc.400}',
+					500: '{zinc.500}',
+					600: '{zinc.600}',
+					700: '{zinc.700}',
+					800: '{zinc.800}',
+					900: '{zinc.900}',
+					950: '{zinc.950}'
 				}
 			},
 			dark: {
 				formField: {
 					hoverBorderColor: '{primary.color}'
+				},
+				surface: {
+					0: '#ffffff',
+					50: '{slate.50}',
+					100: '{slate.100}',
+					200: '{slate.200}',
+					300: '{slate.300}',
+					400: '{slate.400}',
+					500: '{slate.500}',
+					600: '{slate.600}',
+					700: '{slate.700}',
+					800: '{slate.800}',
+					900: '{slate.900}',
+					950: '{slate.950}'
 				}
 			}
 		},
